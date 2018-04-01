cee99703716347fb24f9fd38066af86a10044007
Guy Guidona <mguidona@gmail.com>
2018-04-01 21:18:41 +0100

    Configuration for two players

--- Code/ngay-board.py
+++ Code/ngay-board.py
@@ -1,36 +1,5 @@
-# class Board:
-#     def __init__(self):
-#         self.init()
-
-#     def init(self):
-#         self.cases = [
-#             5,5,5,5,5,
-#             5,5,5,5,5
-#         ]
-    
-#     def printOnScreen(self):
-#         count = 0
-#         for i in self.cases
-#             if(counter == 5)
-#                 print "\n"
-#             print i
-
-counter = 1
-
-cases = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
-grenier = [0, 0, 0, 0, 0]
-
-def printBoard():
-    print('')
-    print('           ',grenier[4],'     ',grenier[3])
-    print('')
-    print('      J   I   H   G   F  ')
-    print('    +---+---+---+---+---+')
-    print('    |',cases[9],'|',cases[8],'|',cases[7],'|',cases[6],'|',cases[5],'|')
-    print(grenier[0],'  +---+---+---+---+---+')
-    print('    |',cases[0],'|',cases[1],'|',cases[2],'|',cases[3],'|',cases[4],'|')
-    print('    +---+---+---+---+---+')
-    print('      A   B   C   D   E  ')
-    print('')
-    print('           ',grenier[1],'     ',grenier[2])
-    print('')
+class Board:
+    def __init__(self):
+        self.init()
+    def init():
+        self.cases
--- Code/ngay-main.py
+++ Code/ngay-main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import os
 
-counter = 1
+counter = 0
 position = 0
 
 cases = [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
-grenier = [0, 0, 0, 0, 0]
-maisons = ["A","B","C","D","E","F","G","H","I","J"]
+grenier = [0, 0]
+maisons = ["A","B","C","D","E","5","4","3","2","1"]
 
 def verifGain(counter, position):
     if((cases[position - 1] == 2) or (cases[position - 1] == 4)):
@@ -15,129 +15,109 @@ def verifGain(counter, position):
         print("gain simple")
         
 def verifPosition(position):
-    if(position > 9):
-        position = 0
-    return position
+    return position % 10
 
 def incCounter(counter):
     counter += 1
-    if(counter > 5):
-        counter = 1
+    return counter % 2
         
+def jeuNord(c):
+    if((c == '1') & (cases[9] > 1)):
+        position = 0
+        for i in range(0, cases[9]):
+            cases[position] += 1
+            position += 1
+            position = position % 10
+        cases[9] = 0
+    if((c == '2') & (cases[8] > 1)):
+        position = 9
+        for i in range(0, cases[8]):
+            cases[position] += 1
+            position += 1
+            position = position % 10
+        cases[8] = 0
+    if((c == '3') & (cases[7] > 1)):
+        position = 8
+        for i in range(0, cases[7]):
+            cases[position] += 1
+            position += 1
+            position = position % 10
+        cases[7] = 0
+    if((c == '4') & (cases[6] > 1)):
+        position = 7
+        for i in range(0, cases[6]):
+            cases[position] += 1
+            position += 1
+            position = position % 10
+        cases[6] = 0
+    if((c == '5') & (cases[5] > 1)):
+        position = 6
+        for i in range(0, cases[5]):
+            cases[position] += 1
+            position += 1
+            position = position % 10
+        cases[5] = 0
+    verifGain(counter, position)
+
+def jeuSud(c):
+    if((c == 'A') & (cases[0] > 1)):
+        position = 1
+        for i in range(0, cases[0]):
+            cases[position] += 1
+            position += 1
+            position = position % 10
+        cases[0] = 0
+    if((c == 'B') & (cases[1] > 1)):
+        position = 2
+        for i in range(0, cases[1]):
+            cases[position] += 1
+            position += 1
+            position = position % 10
+        cases[1] = 0
+    if((c == 'C') & (cases[2] > 1)):
+        position = 3
+        for i in range(0, cases[2]):
+            cases[position] += 1
+            position += 1
+            position = position % 10
+        cases[2] = 0
+    if((c == 'D') & (cases[3] > 1)):
+        position = 4
+        for i in range(0, cases[3]):
+            cases[position] += 1
+            position += 1
+            position = position % 10
+        cases[3] = 0
+    if((c == 'E') & (cases[4] > 1)):
+        position = 5
+        for i in range(0, cases[4]):
+            cases[position] += 1
+            position += 1
+            position = position % 10
+        cases[4] = 0
+    verifGain(counter, position)
+
 def printBoard():
     print('')
-    print('           ',grenier[4],'     ',grenier[3])
+    print('             ',grenier[1])
     print('')
-    print('      J   I   H   G   F  ')
+    print('      1   2   3   4   5  ')
     print('    +---+---+---+---+---+')
     print('    |',cases[9],'|',cases[8],'|',cases[7],'|',cases[6],'|',cases[5],'|')
-    print(grenier[0],'  +---+---+---+---+---+')
+    print('    +---+---+---+---+---+')
     print('    |',cases[0],'|',cases[1],'|',cases[2],'|',cases[3],'|',cases[4],'|')
     print('    +---+---+---+---+---+')
     print('      A   B   C   D   E  ')
     print('')
-    print('           ',grenier[1],'     ',grenier[2])
+    print('             ',grenier[0])
     print('')
 
 while(1):
     printBoard()
-    printString = 'player' + str(counter) + ' >>> '
+    printString = 'player' + str(counter + 1) + ' >>> '
     c = input(printString)
+    if(counter == 0):
+        jeuNord(c)
     if(counter == 1):
-        # if((c == 'A') or (c == 'J')):
-        if((c == 'J') & (cases[9] > 1)):
-            poing = cases[9]
-            position = 0
-            cases[9] = 0
-            for i in range(0, poing):
-                cases[position] += 1
-                position += 1
-                position = verifPosition(position)
-        if((c == 'A') & (cases[0] > 1)):
-            poing = cases[0]
-            position = 1
-            cases[0] = 0
-            for i in range(0, poing):
-                cases[position] += 1
-                position += 1
-                position = verifPosition(position)
-        verifGain(0, position)
-    elif(counter == 2):
-        # if((c == 'B') or (c == 'C')):
-        if((c == 'B') & (cases[1] > 1)):
-            poing = cases[1]
-            position = 2
-            cases[1] = 0
-            for i in range(0, poing):
-                cases[position] += 1
-                position += 1
-                position = verifPosition(position)
-        if((c == 'C') & (cases[2] > 1)):
-            poing = cases[2]
-            position = 3
-            cases[2] = 0
-            for i in range(0, poing):
-                cases[position] += 1
-                position += 1
-                position = verifPosition(position)
-        verifGain(1, position)
-    elif(counter == 3):
-        # if((c == 'D') or (c == 'E')):
-        if((c == 'D') & (cases[3] > 1)):
-            poing = cases[3]
-            position = 4
-            cases[3] = 0
-            for i in range(0, poing):
-                cases[position] += 1
-                position += 1
-                position = verifPosition(position)
-        if((c == 'E') & (cases[4] > 1)):
-            poing = cases[4]
-            position = 5
-            cases[4] = 0
-            for i in range(0, poing):
-                cases[position] += 1
-                position += 1
-                position = verifPosition(position)
-        verifGain(2, position)
-    elif(counter == 4):
-        # if((c == 'F') or (c == 'G')):
-        if((c == 'F') & (cases[5] > 1)):
-            poing = cases[5]
-            position = 6
-            cases[5] = 0
-            for i in range(0, poing):
-                cases[position] += 1
-                position += 1
-                position = verifPosition(position)
-        if((c == 'G') & (cases[6] > 1)):
-            poing = cases[6]
-            position = 7
-            cases[6] = 0
-            for i in range(0, poing):
-                cases[position] += 1
-                position += 1
-                position = verifPosition(position)
-        verifGain(3, position)
-    else:
-        # if((c == 'H') or (c == 'I')):
-        if((c == 'H') & (cases[7] > 1)):
-            poing = cases[7]
-            position = 8
-            cases[7] = 0
-            for i in range(0, poing):
-                cases[position] += 1
-                position += 1
-                position = verifPosition(position)
-        if((c == 'I') & (cases[8] > 1)):
-            poing = cases[8]
-            position = 9
-            cases[8] = 0
-            for i in range(0, poing):
-                cases[position] += 1
-                position += 1
-                position = verifPosition(position)
-        verifGain(4, position)
-    counter += 1
-    if(counter > 5):
-        counter = 1
+        jeuSud(c)
+    counter = incCounter(counter)
