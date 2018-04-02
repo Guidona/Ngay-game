c0a8e33910233f39d39bf6792504a390099e51d6
Guy Guidona <mguidona@gmail.com>
2018-04-03 00:24:23 +0100

    Implementing wining procedures

--- Code/ngayBoard.py
+++ Code/ngayBoard.py
@@ -9,93 +9,127 @@ class Board:
         self.position = 0
         self.grenier = [0, 0]
 
+    def gain(self):
+        self.grenier[self.counter - 1] += self.cases[self.position - 1]
+        self.cases[self.position - 1] = 0
+
+    def incPosition(self):
+        self.position += 1
+        self.position %= 10
+
     def verifGain(self):
+        count = 0
         if((self.cases[self.position - 1] == 2) or (self.cases[self.position - 1] == 4)):
-            self.grenier[self.counter] += self.cases[self.position - 1]
-            self.cases[self.position - 1] = 0
-            print("gain simple")
-            
+            self.gain()
+            count += 1
+        self.incPosition
+        while((self.position != 5) or (self.position != 0)):
+            if((self.cases[self.position - 1] == 2) or (self.cases[self.position - 1] == 4)):
+                self.gain()
+                count += 1
+            else:
+                break
+            self.incPosition  
+        if(count == 1):
+            print("Gain simple")
+        if(count > 1):
+            print("Gain cummulatif")
+                            
     def verifPosition(self):
         return self.position % 10
 
     def incCounter(self):
         self.counter += 1
-        return self.counter % 2
+        self.counter = self.counter % 2
             
-    def jeuNord(self, c):
-        if((c == '1') & (self.cases[9] > 1)):
+    def jeuNord(self):
+        if((self.c == '1') & (self.cases[9] > 1)):
             self.position = 0
             for i in range(0, self.cases[9]):
                 self.cases[self.position] += 1
                 self.position += 1
                 self.position = self.position % 10
             self.cases[9] = 0
-        if((c == '2') & (self.cases[8] > 1)):
+            self.incCounter()
+        elif((self.c == '2') & (self.cases[8] > 1)):
             self.position = 9
             for i in range(0, self.cases[8]):
                 self.cases[self.position] += 1
                 self.position += 1
                 self.position = self.position % 10
             self.cases[8] = 0
-        if((c == '3') & (self.cases[7] > 1)):
+            self.incCounter()
+        elif((self.c == '3') & (self.cases[7] > 1)):
             self.position = 8
             for i in range(0, self.cases[7]):
                 self.cases[self.position] += 1
                 self.position += 1
                 self.position = self.position % 10
             self.cases[7] = 0
-        if((c == '4') & (self.cases[6] > 1)):
+            self.incCounter()
+        elif((self.c == '4') & (self.cases[6] > 1)):
             self.position = 7
             for i in range(0, self.cases[6]):
                 self.cases[self.position] += 1
                 self.position += 1
                 self.position = self.position % 10
             self.cases[6] = 0
-        if((c == '5') & (self.cases[5] > 1)):
+            self.incCounter()
+        elif((self.c == '5') & (self.cases[5] > 1)):
             self.position = 6
             for i in range(0, self.cases[5]):
                 self.cases[self.position] += 1
                 self.position += 1
                 self.position = self.position % 10
             self.cases[5] = 0
+            self.incCounter()
+        else:
+            self.counter = 1
         self.verifGain()
 
-    def jeuSud(self, c):
-        if((c == 'A') & (self.cases[0] > 1)):
+    def jeuSud(self):
+        if((self.c == 'A') & (self.cases[0] > 1)):
             self.position = 1
             for i in range(0, self.cases[0]):
                 self.cases[self.position] += 1
                 self.position += 1
                 self.position = self.position % 10
             self.cases[0] = 0
-        if((c == 'B') & (self.cases[1] > 1)):
+            self.incCounter()
+        elif((self.c == 'B') & (self.cases[1] > 1)):
             self.position = 2
             for i in range(0, self.cases[1]):
                 self.cases[self.position] += 1
                 self.position += 1
                 self.position = self.position % 10
             self.cases[1] = 0
-        if((c == 'C') & (self.cases[2] > 1)):
+            self.incCounter()
+        elif((self.c == 'C') & (self.cases[2] > 1)):
             self.position = 3
             for i in range(0, self.cases[2]):
                 self.cases[self.position] += 1
                 self.position += 1
                 self.position = self.position % 10
             self.cases[2] = 0
-        if((c == 'D') & (self.cases[3] > 1)):
+            self.incCounter()
+        elif((self.c == 'D') & (self.cases[3] > 1)):
             self.position = 4
             for i in range(0, self.cases[3]):
                 self.cases[self.position] += 1
                 self.position += 1
                 self.position = self.position % 10
             self.cases[3] = 0
-        if((c == 'E') & (self.cases[4] > 1)):
+            self.incCounter()
+        elif((self.c == 'E') & (self.cases[4] > 1)):
             self.position = 5
             for i in range(0, self.cases[4]):
                 self.cases[self.position] += 1
                 self.position += 1
                 self.position = self.position % 10
             self.cases[4] = 0
+            self.incCounter()
+        else:
+            self.counter = 0
         self.verifGain()
 
     def printBoard(self):
