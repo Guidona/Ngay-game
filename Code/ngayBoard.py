c17c2246d9efcddd2b8a8fdb449874703a5e6ec0
Guy Guidona <mguidona@gmail.com>
2018-04-06 23:45:32 +0100

    Adding comments and functions refinement

--- Code/ngayBoard.py
+++ Code/ngayBoard.py
@@ -4,7 +4,7 @@ class Board:
 
     def init(self):
         self.c = ' '
-        self.cases = [2, 0, 0, 2, 2, 2, 2, 2, 0, 2]
+        self.cases = [1, 0, 0, 1, 2, 1, 1, 1, 0, 1]
         self.counter = 0
         self.position = 0
         self.grenier = [0, 0]
@@ -17,6 +17,7 @@ class Board:
         self.position += 1
         self.position %= 10
 
+    # Gain verification
     def verifGain(self):
         count = 0
         if((self.cases[self.position - 1] == 2) or (self.cases[self.position - 1] == 4)):
@@ -37,108 +38,60 @@ class Board:
         if(count > 1):
             print("Gain cummulatif")
 
+    # Counter incrementer
     def incCounter(self):
         self.counter += 1
         self.counter %= 2
             
+    # Utility function for distribution
+    def distribution(self, boardPosition, boardCase):
+        self.position = boardPosition
+        for i in range(0, self.cases[boardCase]):
+            self.cases[self.position] += 1
+            self.position += 1
+            self.position = self.position % 10
+        self.cases[boardCase] = 0
+        self.incCounter()
+        self.verifGain()
+
+    # Player twos game
     def jeuNord(self):
         if((self.c == '1') & (self.cases[9] > 1)):
-            self.position = 0
-            for i in range(0, self.cases[9]):
-                self.cases[self.position] += 1
-                self.position += 1
-                self.position = self.position % 10
-            self.cases[9] = 0
-            self.incCounter()
-            self.verifGain()
+            self.distribution(0, 9)
         elif((self.c == '2') & (self.cases[8] > 1)):
-            self.position = 9
-            for i in range(0, self.cases[8]):
-                self.cases[self.position] += 1
-                self.position += 1
-                self.position = self.position % 10
-            self.cases[8] = 0
-            self.incCounter()
-            self.verifGain()
+            self.distribution(9, 8)
         elif((self.c == '3') & (self.cases[7] > 1)):
-            self.position = 8
-            for i in range(0, self.cases[7]):
-                self.cases[self.position] += 1
-                self.position += 1
-                self.position = self.position % 10
-            self.cases[7] = 0
-            self.incCounter()
-            self.verifGain()
+            self.distribution(8, 7)
         elif((self.c == '4') & (self.cases[6] > 1)):
-            self.position = 7
-            for i in range(0, self.cases[6]):
-                self.cases[self.position] += 1
-                self.position += 1
-                self.position = self.position % 10
-            self.cases[6] = 0
-            self.incCounter()
-            self.verifGain()
+            self.distribution(7, 6)
         elif((self.c == '5') & (self.cases[5] > 1)):
-            self.position = 6
-            for i in range(0, self.cases[5]):
-                self.cases[self.position] += 1
-                self.position += 1
-                self.position = self.position % 10
-            self.cases[5] = 0
-            self.incCounter()
-            self.verifGain()
+            self.distribution(6, 5)
         else:
             self.counter = 1
 
+    # Player ones game
     def jeuSud(self):
         if((self.c == 'A') & (self.cases[0] > 1)):
-            self.position = 1
-            for i in range(0, self.cases[0]):
-                self.cases[self.position] += 1
-                self.position += 1
-                self.position = self.position % 10
-            self.cases[0] = 0
-            self.incCounter()
-            self.verifGain()
+            self.distribution(1, 0)
         elif((self.c == 'B') & (self.cases[1] > 1)):
-            self.position = 2
-            for i in range(0, self.cases[1]):
-                self.cases[self.position] += 1
-                self.position += 1
-                self.position = self.position % 10
-            self.cases[1] = 0
-            self.incCounter()
-            self.verifGain()
+            self.distribution(2, 1)
         elif((self.c == 'C') & (self.cases[2] > 1)):
-            self.position = 3
-            for i in range(0, self.cases[2]):
-                self.cases[self.position] += 1
-                self.position += 1
-                self.position = self.position % 10
-            self.cases[2] = 0
-            self.incCounter()
-            self.verifGain()
+            self.distribution(3, 2)
         elif((self.c == 'D') & (self.cases[3] > 1)):
-            self.position = 4
-            for i in range(0, self.cases[3]):
-                self.cases[self.position] += 1
-                self.position += 1
-                self.position = self.position % 10
-            self.cases[3] = 0
-            self.incCounter()
-            self.verifGain()
+            self.distribution(4, 3)
         elif((self.c == 'E') & (self.cases[4] > 1)):
-            self.position = 5
-            for i in range(0, self.cases[4]):
-                self.cases[self.position] += 1
-                self.position += 1
-                self.position = self.position % 10
-            self.cases[4] = 0
-            self.incCounter()
-            self.verifGain()
+            self.distribution(5, 4)
         else:
             self.counter = 0
 
+    # verifDeadlock function to verify if there isn't any dead lock in the game in order to stop
+    def verifDeadlock(self):
+        for i in self.cases:
+            if i > 1:
+                return False
+        return True
+
+    # Function to print the board to the screen
     def printBoard(self):
         print('')
         print('             ',self.grenier[1])
--- Code/ngayMain.py
+++ Code/ngayMain.py
@@ -5,11 +5,19 @@ from ngayBoard import *
 board = Board()
 
 while(1):
+    # Clear the screen in order to have only a single game board in view
     os.system('cls' if os.name == 'nt' else 'clear')
+    # Printing the game board
     board.printBoard()
+    # Verifying if there isn't any deadlock in the game
+    if(board.verifDeadlock() == True):
+        print("Fin de la partie")
+        break
     printString = 'Player ' + str(board.counter + 1) + ' >>> '
     board.c = input(printString)
+    # Player ones game turn
     if(board.counter == 0):
         board.jeuSud()
+    #Player twos game turn
     if(board.counter == 1):
         board.jeuNord()
